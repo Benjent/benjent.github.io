f0825d557fbe32d07b914a44ad9557b79b3f1b41
Benjent <Benjent@hotmail.fr>
2018-03-31 19:54:14 +0200

    Animate nav onpageload

---
+++ assets/public/js/main.js
@@ -0,0 +1,11 @@
+function animateElements() {
+
+    
+    document.querySelectorAll(".fade");
+    
+    document.querySelector("nav").classList.add("move");
+    console.log(document.querySelector("nav"));
+
+}
+
+animateElements();
